3d92f44ea143ecd59a549659c5829622694bfff5
Richard Mable <richardbmable@gmail.com>
2016-01-18 11:57:42 -0500

    added some prototyping to objects to add attributes after instance has been created

--- creatingFirstJSObjects.js
+++ creatingFirstJSObjects.js
@@ -1,5 +1,5 @@
-
 //using a constructor function to create a laptop object
+//this way is more expensive in terms of processes for the computer, but can use it later
 function laptop(memory, brand, screen_size){
 	this.memory = memory;
 	this.brand = brand;
@@ -10,7 +10,7 @@ function laptop(memory, brand, screen_size){
 var my_laptop = new laptop(16, "apple", 15);
 var other_laptop = new laptop(24, "acer", 23);
 
-//preffered to retrieve attributes with dot notation
+//preferred to retrieve attributes with dot notation
 //could use square bracket notation
 alert(my_laptop.memory + "GB");
 alert(other_laptop.brand + "Brand");
@@ -45,6 +45,35 @@ var my_coffee = new coffee_cup("espresso", 16, true);
 
 //access the method via alert function
 alert(my_coffee.your_coffee());
+console.log(my_coffee)
+//adding prototypes to an object
+//this is a way to add attributes to an already created object
+//will not update attributes that have already been set on the object
+//can only add set attributes, not modify the constructor to accept new arguments
+//or anything like that
+//add an attribute with a string
+coffee_cup.prototype.coffee_brand = "green line";
+//add an attribute with a boolean
+coffee_cup.prototype.consumed = false;
+//add an attribute with a function
+coffee_cup.prototype.drunk = function(){
+	//need 'this' to refer back to this instance's particular coffee cup
+	return "Your coffee has been" + (this.consumed ? " consumed!" : " not consumed, get going!");
+};
+
+//call the function added through the prototype
+alert(my_coffee.drunk());
+//print to console so that we can see that changes were made from the pre-prototyping the object
+console.log(my_coffee);
+
+
+
+
+
+
+
+
+
 
 
 
